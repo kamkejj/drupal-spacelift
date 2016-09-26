523df27dbaad676a12500c298224b761d4b72874
Ben McClure <bmcclure@topfloortech.com>
2016-09-26 15:29:14 -0500

    Initial version

---
+++ STARTER/Gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var aluminumCapsule = require('aluminum-capsule');
+
+/**
+ * Load aluminum-capsule which will handle our Gulp tasks.
+ *
+ * Use config.json to override configuration, and create your own tasks here as you see fit.
+ */
+aluminumCapsule.gulpTasks(gulp);
